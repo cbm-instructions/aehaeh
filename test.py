c11db1c6ee562ad8b4b8c917ea3e59e07b110e4f
Eren Saglam <erenando.saglam@gmail.com>
2023-12-06 13:14:34 +0100

    MQTT Client Adjustments

--- test.py
+++ test.py
@@ -5,12 +5,30 @@ from flask_socketio import SocketIO
 # from mfrc522 import SimpleMFRC522
 import sqlite3
 import time
+import paho.mqtt.client as mqtt
+import json
+
+
+def on_message(client, userdata, msg):
+    payload = json.loads(msg.payload.decode())
+    card_id = payload['card_id']
+    print("Message received")
+
+    # Führe deine Reservierungsprüfung durch
+    if card_id != "":
+        response = {'status': 'success', 'message': 'Reservierung vorhanden'}
+    else:
+        response = {'status': 'error', 'message': 'Keine Reservierung gefunden'}
+
+    # Sende die Antwort zurück an den ESP
+    client.publish("response_topic", json.dumps(response))
+
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app)
 
-#os.system('clear')  # clear screen, this is just for the OCD purposes
+# os.system('clear')  # clear screen, this is just for the OCD purposes
 os.system('cls')
 step = 5  # linear steps for increasing/decreasing volume
 
@@ -47,13 +65,15 @@ current_user_values = {
     "Dauer": "",
 }
 
+
 def reset_current_user_values():
-    current_user_values["ID"] = str(id_counter+1)
+    current_user_values["ID"] = str(id_counter + 1)
     current_user_values["Tisch Nr."] = ""
     current_user_values["Datum"] = ""
     current_user_values["Stunde"] = ""
     current_user_values["Minute"] = ""
     current_user_values["Dauer"] = ""
+
 
 # def read_from_rfid():
 #    reader = SimpleMFRC522()
@@ -116,6 +136,8 @@ def read_all_reservations_for_user(user_id):
     finally:
         cursor.close()
         connection.close()
+
+
 def write_reservation_to_database():
     connection = sqlite3.connect("reservations.db")
     cursor = connection.cursor()
@@ -153,6 +175,7 @@ def update_value():
     print("Auto update!")
     socketio.emit('new_value', {'value': 'false'})
 
+
 @socketio.on('update_current_user_values')
 def update_current_user_values(data):
     key = data['key']
@@ -165,6 +188,7 @@ def update_current_user_values(data):
         write_reservation_to_database()
         read_all_reservations_for_user(id_counter)
         reset_current_user_values()
+
 
 # def clkClicked(channel):
 #        global counter
@@ -209,15 +233,21 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-    #create_table_reservations()
-    #current_user_values["ID"] = "2120548"
-    #current_user_values["Tischnummer"] = "4"
-    #current_user_values["Datum"] = "15.12.2022"
-    #current_user_values["Stunde"] = "14"
-    #current_user_values["Minute"] = "30"
-    #current_user_values["Dauer"] = "240"
-    #write_reservation_to_database()
-    #read_all_reservations_for_user("2120548")
+    #app.run(debug=True, host='0.0.0.0')
+    client = mqtt.Client()
+    client.connect("localhost", 1883, 60)
+    client.subscribe("#")
+    client.on_message = on_message
+
+    client.loop_forever()
+    # create_table_reservations()
+    # current_user_values["ID"] = "2120548"
+    # current_user_values["Tischnummer"] = "4"
+    # current_user_values["Datum"] = "15.12.2022"
+    # current_user_values["Stunde"] = "14"
+    # current_user_values["Minute"] = "30"
+    # current_user_values["Dauer"] = "240"
+    # write_reservation_to_database()
+    # read_all_reservations_for_user("2120548")
 
 # GPIO.cleanup()
